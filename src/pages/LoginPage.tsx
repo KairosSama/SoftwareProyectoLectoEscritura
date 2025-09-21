f0e2ad9c864c12f2f6c02e8cbb7f7a2d6a9d1262
GarotinhasEnjoyer <ialvialf@correo.uss.cl>
2025-09-20 23:07:04 -0300

    fix: solo permitir registro de profesores

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -148,8 +148,6 @@ function LoginPage() {
                   className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
                 >
                   <option value="teacher">Profesor</option>
-                  <option value="evaluator">Evaluador</option>
-                  <option value="administrator">Administrador</option>
                 </select>
               </div>
             )}
